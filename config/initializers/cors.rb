16fffcac23bb94783f86aab102d53f07bb1afa42
Phạm Minh Trí <91834683+minhtri2908@users.noreply.github.com>
2025-06-18 15:04:54 +0700

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173','https://test-fe-ruby.onrender.com'
+    origins 'http://localhost:5173','https://test-fe-ruby.vercel.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
